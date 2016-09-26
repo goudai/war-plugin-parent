936261f67519eea7e6bf8524248517a8178e0546
freeman <247687009@qq.com>
2016-09-26 13:43:36 +0800

    https://github.com/goudai/war-plugin-parent.git

--- war-plugin/src/main/java/io/goudai/war/plugin/JSCSSZipHelpr.java
+++ war-plugin/src/main/java/io/goudai/war/plugin/JSCSSZipHelpr.java
@@ -74,6 +74,7 @@ public class JSCSSZipHelpr {
 			throw new RuntimeException(e);
 		}
 	}
+
     private static ErrorReporter create(final File srcFile) {
 		ErrorReporter reporter = new ErrorReporter() {
 			public void warning(String message, String sourceName, int line,
